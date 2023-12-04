25c916b20873b6a2b265f5e2c640743117334f00
vld.lukic@gmail.com <vld.lukic@gmail.com>
2023-12-04 21:54:51 +0100

    Kreiranje

---
+++ Lukovac Fitness/src/views/GalerijaView.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+</script>
+
+<template>
+    <body class="bodyGalerija">
+        
+    
+    <div>
+ <h1>This is Galerija</h1>
+</div>
+</body>
+</template>
+
+<style scoped>
+.bodyGalerija {
+  background-image: url(../assets/Stomak.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 95vh;
+  width: 80%;
+  border-radius: 250px 10px 250px 10px;
+  margin-left: 10%;
+}
+
+h1 {
+  color: white;
+  position: absolute;
+  top: 10%;
+  left: 75%;
+  transform: translateX(-50%);
+  text-align: center;
+  font-size: 50px;
+  font-weight: 500;
+}
+</style>
+
--- Lukovac Fitness/src/views/HomeView.vue
+++ Lukovac Fitness/src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+   <div >
+    
+<div>
+ <!-- <h1>Lukovac Fitness</h1> -->
+</div>
+</div>
 </template>
+
+<style scoped>
+h1 {
+  color: white;
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+  font-size: 50px;
+  font-weight: 500;
+}
+
+
+
+
+</style>
